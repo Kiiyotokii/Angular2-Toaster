1ed260b2dc6a8deb3b3571dad50279c607717ded
Kiiyotokii <112379365+Kiiyotokii@users.noreply.github.com>
2022-12-06 17:47:43 +1030

    Remove dist

--- dist/angular2-toaster/lib/toaster.module.d.ts
+++
@@ -1,5 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-export declare class ToasterModule {
-    static forRoot(): ModuleWithProviders<ToasterModule>;
-    static forChild(): ModuleWithProviders<ToasterModule>;
-}
